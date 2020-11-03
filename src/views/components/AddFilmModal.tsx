7845558f6ead1412e940d0e2df2e5f9dda3ff4ee
Leggga <ivanickii.olegka@gmail.com>
2020-11-03 13:39:13 +0200

    [added] prettier, lint-staged [fixed] reset addFilm modal after submit

--- src/views/components/AddFilmModal.tsx
+++ src/views/components/AddFilmModal.tsx
@@ -18,10 +18,10 @@ interface FormValues extends Omit<FilmTemp, 'release_year'> {
 }
 
 const formItemLayout = {
-  wrapperCol: {xs: {span: 24}}
+  wrapperCol: {xs: {span: 24}},
 }
 const formItemLayoutWithOutLabel = {
-  wrapperCol: {xs: {span: 18, offset: 6}}
+  wrapperCol: {xs: {span: 18, offset: 6}},
 }
 
 const AddFilmModal: React.FC<Props> = ({isVisible, onChangeVisible}) => {
@@ -30,7 +30,10 @@ const AddFilmModal: React.FC<Props> = ({isVisible, onChangeVisible}) => {
 
   const handleSubmit = useCallback(() => {
     const values = form.getFieldsValue()
-    const film: FilmTemp = {...values, release_year: values.release_year.get('year')}
+    const film: FilmTemp = {
+      ...values,
+      release_year: values.release_year.get('year'),
+    }
     dispatch(addFilmRequest(film))
 
     form.resetFields()
@@ -42,55 +45,68 @@ const AddFilmModal: React.FC<Props> = ({isVisible, onChangeVisible}) => {
     form.resetFields()
   }, [onChangeVisible, form])
 
-  const disabledDate = useCallback((date: Moment): boolean =>
-    date.isAfter(moment().endOf('year')) || date.isBefore(new Date(1900, 0))
-    , [])
+  const disabledDate = useCallback(
+    (date: Moment): boolean =>
+      date.isAfter(moment().endOf('year')) ||
+      date.isBefore(new Date(1900, 0)),
+    []
+  )
 
   return (
     <Modal
-      title="Add film"
+      title='Add film'
       visible={isVisible}
       onOk={form.submit}
       onCancel={handleCancel}
     >
       <Form
-        id="filmForm"
+        id='filmForm'
         form={form}
-        name="basic"
+        name='basic'
         requiredMark={false}
         initialValues={{stars: []}}
         onFinish={handleSubmit}
       >
         <Form.Item
-          label="Title"
-          name="title"
-          rules={[{required: true, message: 'Please input title!', whitespace: true}]}
+          label='Title'
+          name='title'
+          rules={[
+            {
+              required: true,
+              message: 'Please input title!',
+              whitespace: true,
+            },
+          ]}
         >
-          <Input/>
+          <Input />
         </Form.Item>
 
         <Form.Item
-          label="Release Year"
-          name="release_year"
+          label='Release Year'
+          name='release_year'
           rules={[{required: true, message: 'Please choose year!'}]}
         >
-          <DatePicker picker="year" disabledDate={disabledDate} onChange={() => console.log('change')}/>
+          <DatePicker
+            picker='year'
+            disabledDate={disabledDate}
+            onChange={() => console.log('change')}
+          />
         </Form.Item>
 
         <Form.Item
-          label="Format"
-          name="format"
+          label='Format'
+          name='format'
           rules={[{required: true, message: 'Please choose format!'}]}
         >
-          <Select placeholder="Please select a format">
-            <Select.Option value="Blu-Ray">Blu-Ray</Select.Option>
-            <Select.Option value="DVD">DVD</Select.Option>
-            <Select.Option value="VHS">VHS</Select.Option>
+          <Select placeholder='Please select a format'>
+            <Select.Option value='Blu-Ray'>Blu-Ray</Select.Option>
+            <Select.Option value='DVD'>DVD</Select.Option>
+            <Select.Option value='VHS'>VHS</Select.Option>
           </Select>
         </Form.Item>
 
         <Form.List
-          name="stars"
+          name='stars'
           rules={[
             {
               validator: async (_, stars) => {
@@ -104,26 +120,49 @@ const AddFilmModal: React.FC<Props> = ({isVisible, onChangeVisible}) => {
           {(fields, {add, remove}, {errors}) => (
             <>
               {fields.map((field, index) => (
-                <Space key={field.key} style={{display: 'flex', marginBottom: 8, flex: 1}} align="baseline">
+                <Space
+                  key={field.key}
+                  style={{
+                    display: 'flex',
+                    marginBottom: 8,
+                    flex: 1,
+                  }}
+                  align='baseline'
+                >
                   <Form.Item
                     {...field}
-                    {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
-                    rules={[{required: true, message: 'Missing full name', whitespace: true}]}
+                    {...(index === 0
+                      ? formItemLayout
+                      : formItemLayoutWithOutLabel)}
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Missing full name',
+                        whitespace: true,
+                      },
+                    ]}
                     validateTrigger={['onChange', 'onBlur']}
                     label={index === 0 ? 'Stars' : ''}
                     key={field.key}
                   >
-                    <Input placeholder="Full name"/>
+                    <Input placeholder='Full name' />
                   </Form.Item>
 
-                  <MinusCircleOutlined onClick={() => remove(field.name)}/>
+                  <MinusCircleOutlined
+                    onClick={() => remove(field.name)}
+                  />
                 </Space>
               ))}
               <Form.Item>
-                <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined/>}>
+                <Button
+                  type='dashed'
+                  onClick={() => add()}
+                  block
+                  icon={<PlusOutlined />}
+                >
                   Add star
                 </Button>
-                <Form.ErrorList errors={errors}/>
+                <Form.ErrorList errors={errors} />
               </Form.Item>
             </>
           )}
--- src/views/components/FilmsTable.tsx
+++ src/views/components/FilmsTable.tsx
@@ -5,14 +5,19 @@ import {Table, Tag} from 'antd'
 import Button from 'antd/es/button'
 import Column from 'antd/es/table/Column'
 //types
-import {SorterResult, TableCurrentDataSource} from 'antd/lib/table/interface'
-import {FilmsFormat, Film} from '@/store/ducks/films/types'
+import {
+  SorterResult,
+  TableCurrentDataSource,
+} from 'antd/lib/table/interface'
+import {Film, FilmsFormat} from '@/store/ducks/films/types'
 //redux
-import {selectFilmsByConditions, selectIsLoading} from '@/store/ducks/films/selectors'
+import {
+  selectFilmsByConditions,
+  selectIsLoading,
+} from '@/store/ducks/films/selectors'
 import {removeFilmRequest, setSortOrder} from '@/store/ducks/films/actions'
 //utils
 import {getFormatFilmColor} from '@/utils'
-
 
 const FilmsTable: React.FC = () => {
   const dispatch = useDispatch()
@@ -20,34 +25,63 @@ const FilmsTable: React.FC = () => {
   const films: Film[] = useSelector(selectFilmsByConditions)
 
   //handlers
-  const handleSortByTitle = useCallback((_, __, sorter: SorterResult<Film> | SorterResult<Film>[], extra: TableCurrentDataSource<Film>) => {
-    if (extra.action === 'sort' && !(sorter instanceof Array)) {
-      dispatch(setSortOrder(sorter.order))
-    }
-  }, [dispatch])
+  const handleSortByTitle = useCallback(
+    (
+      _,
+      __,
+      sorter: SorterResult<Film> | SorterResult<Film>[],
+      extra: TableCurrentDataSource<Film>
+    ) => {
+      if (extra.action === 'sort' && !(sorter instanceof Array)) {
+        dispatch(setSortOrder(sorter.order))
+      }
+    },
+    [dispatch]
+  )
 
-  const handleRemove = useCallback((record: Film) => dispatch(removeFilmRequest(record.id)), [dispatch])
+  const handleRemove = useCallback(
+    (record: Film) => dispatch(removeFilmRequest(record.id)),
+    [dispatch]
+  )
 
   //columns render
-  const renderFormatTag = useCallback((format: FilmsFormat) =>
-    <Tag color={getFormatFilmColor(format)} key={format}>{format}</Tag>, [])
-  const renderStars = useCallback((stars: string[]) => <>{stars.join(', ')}</>, [])
-  const renderAction = useCallback((_, record) =>
-    <Button onClick={() => handleRemove(record)}>Remove</Button>, [handleRemove])
+  const renderFormatTag = useCallback(
+    (format: FilmsFormat) => (
+      <Tag color={getFormatFilmColor(format)} key={format}>
+        {format}
+      </Tag>
+    ),
+    []
+  )
+  const renderStars = useCallback(
+    (stars: string[]) => <>{stars.join(', ')}</>,
+    []
+  )
+  const renderAction = useCallback(
+    (_, record) => (
+      <Button onClick={() => handleRemove(record)}>Remove</Button>
+    ),
+    [handleRemove]
+  )
 
   return (
     <Table
-      rowKey="id"
+      rowKey='id'
       dataSource={films}
-      tableLayout="fixed"
+      tableLayout='fixed'
       loading={isLoading}
       onChange={handleSortByTitle}
     >
-      <Column title="Title" dataIndex="title" sorter={true}/>
-      <Column title="Release Year" dataIndex="release_year"/>
-      <Column title="Format" dataIndex="format" render={renderFormatTag}/>
-      <Column title="Stars" dataIndex="stars" render={renderStars}/>
-      <Column title="Action" dataIndex="Action" align="center" render={renderAction}/>
+      <Column title='Title' dataIndex='title' sorter={true} />
+      <Column title='Release Year' dataIndex='release_year' />
+      <Column title='Format' dataIndex='format' render={renderFormatTag} />
+      <Column title='Stars' dataIndex='stars' render={renderStars} />
+      <Column
+        title='Action'
+        dataIndex='Action'
+        align='center'
+        render={renderAction}
+      />
     </Table>
   )
 }
